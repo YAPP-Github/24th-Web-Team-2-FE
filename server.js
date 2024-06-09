6e8a13e0a39dd0c7775b604f69296ad6ea6a1801
dladncks1217 <dlaxodud1217@gmail.com>
2024-06-09 19:50:27 +0900

    chore: Node.js 서버 https 대응 임시 제거

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
-const https = require("https");
-const fs = require("fs");
+// const https = require('https');
+const http = require('http');
+const fs = require('fs');
 
-const { parse } = require("url");
+const { parse } = require('url');
 
-const next = require("next");
+const next = require('next');
 
-const isDev = process.env.NEXT_PUBLIC_NODE_ENV !== "production";
+const isDev = process.env.NEXT_PUBLIC_NODE_ENV !== 'production';
 const PORT = 3000;
-const app = next({ dev: isDev, hostname: "localhost", port: PORT });
+const app = next({ dev: isDev, hostname: 'localhost', port: PORT });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const options = {
-    // key: fs.readFileSync("./privkey.pem"),
-    // cert: fs.readFileSync("./fullchain.pem"),
+    // key: fs.readFileSync('./localhost+1-key.pem'),
+    // cert: fs.readFileSync('./localhost+1.pem'),
   };
 
-  https
+  // https
+  //   .createServer(options, (req, res) => {
+  //     const parsedURL = parse(req.url, true);
+  //     handle(req, res, parsedURL);
+  //   })
+  //   .listen(PORT, err => {
+  //     if (err) throw err;
+  //     console.log(`running server on https://localhost:${PORT}`);
+  //   });
+
+  http
     .createServer(options, (req, res) => {
       const parsedURL = parse(req.url, true);
       handle(req, res, parsedURL);
     })
-    .listen(PORT, (err) => {
+    .listen(PORT, err => {
       if (err) throw err;
-      console.log(`running server on https:localhost:${PORT}`);
+      console.log(`running server on https://localhost:${PORT}`);
     });
 });
