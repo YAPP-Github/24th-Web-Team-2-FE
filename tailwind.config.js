addb27debdb1e4e282a9733676cc1c98ea5e24d8
moonki <moonki0623@naver.com>
2024-06-27 00:58:24 +0900

    feat: text style & color 디자인시스템 config 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,30 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
     path.join(__dirname, './src/**/*.(js|jsx|ts|tsx)'),
   ],
-
   theme: {
-    extend: {},
+    colors: {
+      black: '#000000',
+      darkgrey: '#797979',
+      lightgrey: '#DBDBDB',
+      background_grey: '#F4F4F4',
+      white: '#ffffff',
+      blue: '#168FD0',
+      green: '#69B012',
+      violet: '#862872',
+      red: '#C62D32',
+      orange: '#E68302',
+      yellow: '#F7BB13',
+    },
+    letterSpacing: {
+      tightest: '-5%',
+      tighter: '-4%',
+    },
+    extend: {
+      fontSize: {
+        '21px': '1.3125rem',
+        '15px': '0.9375rem',
+      },
+    },
   },
   plugins: [],
 };
