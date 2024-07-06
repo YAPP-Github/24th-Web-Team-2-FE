268f3ce0cbebc2e2f9b05203f7cf0305e8461e81
moonki <moonki0623@naver.com>
2024-07-06 16:37:36 +0900

    feat: Header UI 변경 반영

---
+++ src/assets/icons/HomeIcon.tsx
@@ -0,0 +1,27 @@
+import { IconProps } from '@/types/icon';
+
+const HomeIcon = ({ width, height, fill, className, onClick }: IconProps) => {
+  return (
+    <svg
+      width={width}
+      height={height ?? width}
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      className={className}
+      onClick={onClick}
+    >
+      <g id='icn'>
+        <path
+          id='Vector'
+          d='M6 19.0009H9.692V13.1169H14.308V19.0009H18V10.0009L12 5.46289L6 10.0009V19.0009ZM5 20.0009V9.50089L12 4.21289L19 9.50089V20.0009H13.308V14.1169H10.692V20.0009H5Z'
+          fill={fill || 'black'}
+          stroke={fill || 'black'}
+          stroke-width='0.5'
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default HomeIcon;
--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -1,3 +1,4 @@
+import HomeIcon from '@/assets/icons/HomeIcon';
 import ServiceIcon from '@/assets/icons/ServiceIcon';
 
 interface HeaderProps {
@@ -6,9 +7,16 @@ interface HeaderProps {
 
 export const Header = ({ isMain }: HeaderProps) => {
   return (
-    <div className={`${isMain ? 'py-8' : 'py-6'} px-6 flex items-center justify-between w-full bg-white`}>
-      <ServiceIcon width={isMain ? 250 : 150} />
-      {isMain && <span className='cursor-pointer text-body3'>My Page</span>}
+    <div
+      className={`${isMain ? 'py-8 justify-between' : 'py-6 justify-center'} px-6 flex items-center w-full bg-white`}
+    >
+      <ServiceIcon width={isMain ? 195 : 156} />
+      {isMain && (
+        <span className='flex flex-row items-center gap-1'>
+          <HomeIcon width={24} />
+          <span className='cursor-pointer text-body3'>My Page</span>
+        </span>
+      )}
     </div>
   );
 };
