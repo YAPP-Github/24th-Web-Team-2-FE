a87a6fee44eb648af835219f4349d2aa2e9a81c6
moonki <moonki0623@naver.com>
2024-07-07 14:38:07 +0900

    feat: Chip implemented

---
+++ src/stories/Chip.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { Chip } from './Chip';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Example/Chip',
+  component: Chip,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {},
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: {},
+} satisfies Meta<typeof Chip>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Activated: Story = {
+  args: {
+    text: '경제',
+    isActivated: true,
+  },
+};
+
+export const Deactivated: Story = {
+  args: {
+    text: 'IT',
+    isActivated: false,
+  },
+};
---
+++ src/stories/Chip.tsx
@@ -0,0 +1,18 @@
+interface ChipProps {
+  /**
+   * text to display
+   */
+  text: string;
+  /**
+   * isActivated
+   */
+  isActivated?: boolean;
+}
+
+export const Chip = ({ text, isActivated = true }: ChipProps) => {
+  return (
+    <span className={`${isActivated ? 'border-black' : 'border-darkgrey'} px-2 border rounded-3xl text-caption`}>
+      {text}
+    </span>
+  );
+};
