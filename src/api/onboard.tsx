22829114b40e556d1fbf5a3d7a5929ef785d707f
moonki <moonki0623@naver.com>
2024-07-10 22:31:03 +0900

    chore: 파일명 tsx->ts 변경 / 콘솔 제거

--- src/api/onboard.tsx
+++
@@ -1,7 +0,0 @@
-import { GET } from '@/network';
-
-export const getInterestList = async () => {
-  const data = await GET('/interests');
-  console.log('get interest', data.data);
-  return data.data;
-};
