f519d5ef5cc78004123f2692d88b4622e27096f3
dladncks1217 <dlaxodud1217@gmail.com>
2024-08-04 00:42:52 +0900

    feat: tanstackQuery 도입

--- src/api/onboard.ts
+++ src/api/onboard.ts
@@ -1,6 +1,24 @@
 import { GET } from '@/network';
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+export interface Interest {
+  id: string;
+  interest: string;
+  desc: string;
+}
+
+interface InterestListData {
+  data: Interest[];
+}
 
 export const getInterestList = async () => {
-  const data = await GET('/interests');
+  const data = <InterestListData>await GET('/interests');
+
   return data.data;
+};
+
+export const useGetInterestList = () => {
+  const result = useSuspenseQuery({ queryFn: getInterestList, queryKey: ['interestList'] });
+
+  return { data: result.data ? result.data : [] };
 };
