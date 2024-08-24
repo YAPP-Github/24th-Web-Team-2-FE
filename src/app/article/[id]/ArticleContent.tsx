f24fa43a8bcdabd99d864bc2feb2315308f81467
dladncks1217 <dlaxodud1217@gmail.com>
2024-08-25 02:47:30 +0900

    feat: unreadMail api 연동 코드 추가

--- src/app/article/[id]/ArticleContent.tsx
+++ src/app/article/[id]/ArticleContent.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { MailDataType } from '@/api/hooks/useUnreadQuery';
 import ArticleHeader from '@/components/Article/ArticleHeader';
 import { ArticleType } from '@/types';
 import useIntersectionObserver from '@/utils/hooks/useIntersectionObserver';
@@ -7,40 +8,24 @@ import { useEffect, useState } from 'react';
 
 interface ArticleContentProps {
   isToday: boolean;
-  articleId: string;
+  mailData: MailDataType;
 }
 
-const ArticleContent = ({ isToday, articleId }: ArticleContentProps) => {
-  const [articleData, setArticleData] = useState<ArticleType>();
+const ArticleContent = ({ isToday, mailData }: ArticleContentProps) => {
   const [headerType, setHeaderType] = useState<'default' | 'simplified'>('default');
 
-  useEffect(() => {
-    getArticleData(articleId).then(data => setArticleData(data));
-  }, [articleId]);
-
   return (
-    articleData && (
-      <div id={articleId} className='flex flex-col items-center w-full gap-2'>
-        <ArticleHeader {...articleData} headerType={headerType} />
+    <div id={mailData.mailId} className='flex flex-col items-center w-full gap-2'>
+      <ArticleHeader {...mailData} headerType={headerType} />
 
-        <div className='w-content h-[1200px] bg-blue'></div>
-      </div>
-    )
+      {/* <div className='w-content h-[1200px] bg-blue'></div> */}
+      <div
+        style={{ width: '100%', overflow: 'scroll' }}
+        className='w-content h-[1200px]'
+        dangerouslySetInnerHTML={{ __html: mailData.payload[0].body }}
+      ></div>
+    </div>
   );
 };
 
 export default ArticleContent;
-
-const getArticleData = async (id: string) => {
-  return {
-    id: 'articleId1',
-    title: '국가안전보장회의',
-    type: 'IT/테크',
-    date: '2024-06-14',
-    from: {
-      domain: 'official@git',
-      profile: 'https://picsum.photos/36',
-    },
-    group: '채민이의 유용한 아티클',
-  };
-};
