db41e4c73ffe27961f0b5bddefa6b117d53400cd
moonki <moonki0623@naver.com>
2024-08-28 01:04:18 +0900

    feat: Not Found page implemented

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import NotfoundImage from '@/assets/images/notfound.png';
+
+const Test = () => {
+  return (
+    <div className='flex flex-col items-center justify-center w-screen h-screen gap-6'>
+      <Image src={NotfoundImage} alt='Not Found' />
+      <div className='flex flex-col items-center gap-3'>
+        <h1 className='text-black text-h1'>어머 어디가세요?</h1>
+        <Link href='/' className='text-blue text-btn1'>
+          홈으로 돌아가기
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Test;
