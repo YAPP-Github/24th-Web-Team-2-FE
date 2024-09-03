8e74ea9a920d6e283b59bffcebcf45f6605826b4
moonki <moonki0623@naver.com>
2024-09-04 00:21:37 +0900

    feat: TOS / PP 추가

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import Pp from '@/app/privacy-policy/pp.md';
+
+const TosPage = () => {
+  return (
+    <div className='py-10 prose'>
+      <Pp />
+    </div>
+  );
+};
+export default TosPage;
---
+++ src/app/terms-of-service/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import Tos from '@/app/terms-of-service/tos.md';
+
+const TosPage = () => {
+  return (
+    <div className='py-10 prose'>
+      <Tos />
+    </div>
+  );
+};
+export default TosPage;
